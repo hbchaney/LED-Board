d21e3332d32547f1867f5c9720fed2e8a50447e5
hbchaney <hbchaney@gmail.com>
2022-04-24 21:46:39 -0400

    pre commit

--- Animation_engine/codes_dump/ex.cpp
+++ Animation_engine/codes_dump/ex.cpp
@@ -1,13 +1,22 @@
 #include<array> 
 #include<vector> 
+#include<tuple> 
+#include<list>
 #include<iostream>
 using namespace std; 
 
 int main () { 
     array<int,5> Harrison {1,2,3,4,8};
-    cout << Harrison.max_size() << endl; 
-    cout << Harrison.data() << endl; 
+    // cout << Harrison.max_size() << endl; 
+    // cout << Harrison.data() << endl; 
 
     vector<int> Harry; 
+    tuple<int> Harrin; 
+    list<int> Zane; 
+    Zane = {1,2,56,87,9,3,5}; 
+    cout << Zane << endl; 
+
+
+
     return 0; 
 }
