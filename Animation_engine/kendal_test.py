380e9fca1f7beddb57bcb65bffda82a36458fa42
bxchaney <bxchaney@gmail.com>
2022-05-08 22:00:09 -0400

    LEARN HOW TO OPTIMIZE YOUR CODE WITH THIS ONE SIMPLE TRICK!! DEVS HATE THAT HE IS SHARING THIS SECRET!!

--- Animation_engine/kendal_test.py
+++ Animation_engine/kendal_test.py
@@ -1,3 +1,4 @@
+from math import sqrt
 # Program to check if a number is prime or not
 
 num = 29
@@ -11,7 +12,8 @@ flag = False
 # prime numbers are greater than 1
 if num > 1:
     # check for factors
-    for i in range(2, num):
+    for i in range(2, int(sqrt(num))): #only necessary to check up to sqrt of n to determine if integer is prime
+                                       #result is O(n^.5) time complexity vs O(n)
         if (num % i) == 0:
             # if factor is found, set flag to True
             flag = True
